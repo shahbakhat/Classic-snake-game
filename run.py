0a9e82f8288ba27e87d6fe692c377857e740a654
Shah Bakhat <shahbakhat008@gmail.com>
2023-03-21 20:18:45 +0000

    set up a new terminal window

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import curses
+
+# Setting up the window
+curses.initscr()
+window = curses.newwin(20,60,0,0) # 1st Y and then X
+window.keypad(1)
+
+curses.noecho(0) # To not hear any other inputs
+curses.curs_set(0)
+window.border(0)
+window.nodelay(1)
+
+
