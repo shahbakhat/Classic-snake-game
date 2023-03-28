14464161696fca5de713e4356075eab2e775682b
Shah Bakhat <shahbakhat008@gmail.com>
2023-03-28 11:05:15 +0100

    defined the width and height of the window and made comments more simple

--- run.py
+++ run.py
@@ -1,42 +1,40 @@
-import curses
+import curses 
 from random import randint
 
-# Setting up the window
+#constants
+
+width = 18
+height = 58 
+
+# setup window
 curses.initscr()
-win = curses.newwin(20,60,0,0)
+win = curses.newwin(width+2, height+2, 0, 0) # y,x
 win.keypad(1)
 curses.noecho()
 curses.curs_set(0)
 win.border(0)
-win.nodelay(1)
+win.nodelay(1) # -1
 
-# Snake and Food
-snake = [(4,10),(4,9),(4,8)]
-food = (6 , 6)
+# snake and food
+snake = [(4, 10), (4, 9), (4, 8)]
+food = (10, 15)
 
-win.addch(food[0], food[1] , '#')
-
-# Game logic
+win.addch(food[0], food[1], '#')
+# game logic
 score = 0
 
 ESC = 27
 key = curses.KEY_RIGHT
 
 while key != ESC:
-    win.addstr(0, 2, 'Score' + ' '  + ' = ' + ' ' +  str(score))
-    #increase speed of the snake
-    win.timeout(150 - (len(snake)) // 5 + len(snake) // 10 % 120 )
-
+    win.addstr(0, 2, 'Score ' + str(score) + ' ')
+    win.timeout(150 - (len(snake)) // 5 + len(snake)//10 % 120) # increase speed
 
     prev_key = key
     event = win.getch()
-    key = event if event !=-1 else prev_key
+    key = event if event != -1 else prev_key
 
-
-
-
-    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT,
-                curses.KEY_UP, curses.KEY_DOWN, ESC] :
+    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
         key = prev_key
 
     # calculate the next coordinates
@@ -51,24 +49,23 @@ while key != ESC:
     if key == curses.KEY_RIGHT:
         x += 1
 
-    # New head of the snake
-    snake.insert(0,(y,x))
-    # if snake hits the border, then
+    snake.insert(0, (y, x)) # append O(n)
+
+    # check if we hit the border
     if y == 0: break
-    if y == 20 - 1: break
+    if y == width+1: break
     if x == 0: break
-    if x == 60 - 1: break
+    if x == height+1: break
 
-    # when snake hits his his own body
-    if snake[0] in snake[1:]:break
+    # if snake runs over itself
+    if snake[0] in snake[1:]: break
 
-    # when snake eats the food
-    if snake [0] == food:
+    if snake[0] == food:
+        # eat the food
         score += 1
         food = ()
         while food == ():
-            food = (randint,(1,18), randint(1,58))
-
+            food = (randint(1,width), randint(1,height))
             if food in snake:
                 food = ()
         win.addch(food[0], food[1], '#')
@@ -77,6 +74,7 @@ while key != ESC:
         last = snake.pop()
         win.addch(last[0], last[1], ' ')
 
-    win.addch(snake[0][0], snake[0][1] , ' * ')
+    win.addch(snake[0][0], snake[0][1], '*')
+
 curses.endwin()
 print(f"Final score = {score}")
