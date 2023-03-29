4a2a6ab85e3b46e017408665d919a751194627d6
Shah Bakhat <shahbakhat008@gmail.com>
2023-03-29 15:55:51 +0100

    cleaned up the syntax

--- run.py
+++ run.py
@@ -4,16 +4,16 @@ from random import randint
 #constants
 
 width = 18
-height = 58 
+height = 58
 
 # setup window
 curses.initscr()
-win = curses.newwin(width+2, height+2, 0, 0) # y,x
+win = curses.newwin(width + 2, height + 2, 0, 0) # y,x
 win.keypad(1)
 curses.noecho()
 curses.curs_set(0)
 win.border(0)
-win.nodelay(1) # -1
+win.nodelay(1) # - 1
 
 # snake and food
 snake = [(4, 10), (4, 9), (4, 8)]
@@ -28,13 +28,15 @@ key = curses.KEY_RIGHT
 
 while key != ESC:
     win.addstr(0, 2, 'Score ' + str(score) + ' ')
-    win.timeout(150 - (len(snake)) // 5 + len(snake)//10 % 120) # increase speed
+    win.timeout(150 - (len(snake)) // 5 
+    + len(snake)//10 % 120) # increase speed
 
     prev_key = key
     event = win.getch()
-    key = event if event != -1 else prev_key
+    key = event if event != - 1 else prev_key
 
-    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
+    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT,
+    curses.KEY_UP, curses.KEY_DOWN, ESC]:
         key = prev_key
 
     # calculate the next coordinates
@@ -53,9 +55,9 @@ while key != ESC:
 
     # check if we hit the border
     if y == 0: break
-    if y == width+1: break
+    if y == width + 1: break
     if x == 0: break
-    if x == height+1: break
+    if x == height + 1: break
 
     # if snake runs over itself
     if snake[0] in snake[1:]: break
@@ -74,7 +76,7 @@ while key != ESC:
         last = snake.pop()
         win.addch(last[0], last[1], ' ')
 
-    win.addch(snake[0][0], snake[0][1], '*')
+    win.addch(snake[0][0], snake[0][1], ' * ')
 
 curses.endwin()
 print(f"Final score = {score}")
